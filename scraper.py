3d5b34b63a3d621ecaa6fa62d22a7541aa145d8f
wiseman <jjwiseman@gmail.com>
2012-06-11 10:51:43 -0700

    Fix misspellings.

--- scraper.py
+++ scraper.py
@@ -1341,7 +1341,7 @@ class EmbedlyOEmbed(OEmbed):
     api_params = {'format':'json', 'maxwidth':600, 'key' : "embedly_api_key" }
  
 class GenericScraper(MediaScraper):
-    """a special scrapper not associated with any domains, used to
+    """a special scraper not associated with any domains, used to
        write media objects to links by hand"""
     domains = ['*']
     height = 480
